a7154331d42acdb1cd08c5abf611c4b9b2226753
816601 <816601@SAIT224158IT.ACDM.DS.SAIT.CA>
2021-10-17 21:41:43 -0600

    added code to user and accountservice classes

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -5,10 +5,45 @@
  */
 package models;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 816601
  */
-public class User {
+public class User implements Serializable {
+    private String username;
+    private String password;
+    
+    public User() {
+        this.username = "";
+        this.password = "";
+    }
+    
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    @Override
+    public String toString() {
+        return username;
+    }
     
 }
--- src/java/services/AccountService.java
+++ src/java/services/AccountService.java
@@ -5,10 +5,19 @@
  */
 package services;
 
+import models.User;
+
 /**
  *
  * @author 816601
  */
 public class AccountService {
     
+    public User login(String username, String password)
+    {
+        if ((username.equals("abe") || username.equals("barb")) && password.equals("password"))
+            return new User(username, null);
+        
+        return null;
+    } 
 }
